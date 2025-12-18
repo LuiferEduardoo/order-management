5f848e2d776f9e0ff169332d3a10e653d169c993
LuiferEduardoo <luifer01ortegaperez@gmail.com>
2025-12-17 22:06:16 -0500

    feat: enable cors

--- src/main.ts
+++ src/main.ts
@@ -16,6 +16,7 @@ async function bootstrap() {
   app.useGlobalPipes(
     new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
   );
+  app.enableCors({ origin: true });
 
   await app.listen(process.env.PORT ?? 3000);
 }
